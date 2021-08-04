3090349977b1ec168e25ee6694f585697ae8d959
Leyght <savier811@gmail.com>
2021-08-04 12:10:02 +1200

    About Page finished

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import "./assets/styles/Containers.css";
 import "./assets/styles/Transitions.css";
 
 import Home from "./components/Pages/Home";
+import About from "./components/Pages/About";
 
 import Navbar from "./components/Blocks/Navbar"
 import Footer from "./components/Blocks/Footer";
@@ -52,15 +53,6 @@ export default function App() {
             </Router>
         </div>
         
-    )
-}
-
-
-function About() {
-    return (
-        <div>
-            <h1>About</h1>
-        </div>
     )
 }
 
---
+++ src/components/Pages/About.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "../../assets/styles/Style.css";
+import "../../assets/styles/About.css"
+
+export default function About() {
+    return(
+        <div className="container">
+            <div className="row">
+                <div id="AboutLeft" className="col">
+                    <span className="Header Header2">About Us</span>
+                    <br />
+                    <br />
+                    Prominence Curtains and Blinds has been a leading retailer of window treatments in Whanganui since 2007. Our wide selection of affordable home decor products and accessories provide the perfect ways to personalize any home. We make it easy for our customers to add their own unique styles to their living spaces.
+                    <br />
+                    We take pride in using our expert eye for creativity to help our customers hone theirs.  We have 30 years experience in the world of blinds and curtains and are committed to finding the right products to meet any decorating vision.
+                    <br />
+                    Prominence Curtains and Blinds is a mobile service, based in Whanganui, and will come to you for an obligation-free measure and quote.
+                    <br />
+                    Get In Touch
+                </div>
+                <div id="AboutRight" className="col">
+                </div>
+            </div>
+        </div>
+    )
+}
